baade7c97ec868a0b188ef340f846ae4b7456e9b
Jeremy Giraudet <jeremy.giraudet@immutable.com>
2024-06-17 11:52:38 +0200

    Update theme with main colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,19 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: "#121213",
+      grey: {
+        100: "#F3F6F8",
+        200: "#D9D9D9",
+      },
+      white: "#F3F6F8",
+      blue: "#6398FF",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
