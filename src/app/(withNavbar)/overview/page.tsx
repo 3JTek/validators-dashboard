a02b1fe7e4102b0b54feef478647c3b96f1e8391
Jeremy Giraudet <jeremy.giraudet@immutable.com>
2024-06-17 17:29:47 +0200

    Create dummy overview page

--- src/app/(withNavbar)/overview/page.tsx
+++ src/app/(withNavbar)/overview/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Overview() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Overview</main>;
+  return (
+    <main>
+      <h1 className="text-center font-bold">Overview page in construction 👷‍♂️</h1>
+    </main>
+  );
 }
