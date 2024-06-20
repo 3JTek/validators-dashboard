44ed09a31211cd71b92cd3bf53d817a45244301d
Jeremy Giraudet <jeremy.giraudet@immutable.com>
2024-06-20 09:30:53 +0200

    Center overview page

--- src/app/(withNavbar)/overview/page.tsx
+++ src/app/(withNavbar)/overview/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main>
-      <h1 className="text-center font-bold">Overview page in construction 👷‍♂️</h1>
+      <div className="flex h-96 w-full items-center justify-center">
+        <h1 className="font-bold">Overview page in construction 👷‍♂️</h1>
+      </div>
     </main>
   );
 }
